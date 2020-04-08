791db46416146d146c92cf6969f9572fedbc2382
Jamie Donohoe <S00189802@mail.itsligo.ie>
2020-04-08 21:45:56 +0100

    Adding funtionality to task tracker

---
+++ TimetableIdea/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimetableIdea
+{
+    public class Task
+    {
+        //props
+        public string Name { get; set; }
+        public string Goal { get; set; }
+        public string Subject { get; set; }
+        public DateTime TimeCreated { get; set; }
+
+        //constructor
+        public Task(string name, string goal, string subject, DateTime timeCreated)
+        {
+            Name = name;
+            Goal = goal;
+            Subject = subject;
+            TimeCreated = timeCreated;
+        }
+
+    }
+}
